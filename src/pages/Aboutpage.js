57ba67c0ae68babbbc55af06042b401d49acd9b7
Alok8777 <alokprasad85077@gmail.com>
2021-01-11 10:50:14 +0530

    first commit

---
+++ src/pages/Aboutpage.js
@@ -0,0 +1,22 @@
+const Aboutpage = () => {
+    return (
+      <div className="about">
+        <h3 className="about__title">ABOUT ME</h3>
+        <div className="about__info">
+          <p>ALOK PRASAD  </p>
+          <div>
+            <p>A mern stack developer with problem solving skills </p>
+            <ul>
+              <li>Love Coding </li>
+              <li>Writing codes give  pleasure to me </li>
+              <li>Looking forward to work in a innovative place </li>
+              <li>Love playing cricket </li>
+              <li>AB De Villiers is my favourite Cricketer </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Aboutpage;
